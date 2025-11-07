e29f46e48837c5379ecf6ee6cb9149386d9653c6
NikaDeg <garelskaiavv1@gmail.com>
2025-11-07 16:52:15 +0200

    new

---
+++ Module1/Task8.js
@@ -0,0 +1,16 @@
+'use strict';
+const start_year_str = prompt('Enter start year: ');
+const start_year = parseInt(start_year_str);
+const end_year_str = prompt('Enter end year: ');
+const end_year = parseInt(end_year_str);
+const all_leap_years = document.querySelector("#years");
+
+for (let i = start_year; i <= end_year; i++ ){
+    if (i % 4 === 0 && i % 100 !== 0 || i % 400 === 0){
+        const li = document.createElement('li');
+        li.textContent = i;
+        all_leap_years.appendChild(li);
+}
+}
+
+
