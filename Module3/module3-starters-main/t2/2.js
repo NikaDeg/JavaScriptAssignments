a60fc093ce7cd54634022c0ed4d07d9745da0ffb
NikaDeg <garelskaiavv1@gmail.com>
2025-11-18 10:08:00 +0200

    new

--- Module3/module3-starters-main/t2/2.js
+++ Module3/module3-starters-main/t2/2.js
@@ -1,11 +1,4 @@
-// Open t2 folder in your IDE/editor. Add HTML by using createElement() and appendChild methods. (2p)
-// Add the following HTML code to the element with id="target"
-// <li>First item</li>
-// <li>Second item</li>
-// <li>Third item</li>
-// Add class my-item to the second list item
 'use strict';
-
 const div = document.querySelector('#target');
 const items = ['First Item', 'Second Item', 'Third Item'];
 
