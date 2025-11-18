b060585355da799197527738cf12aaa049954f9c
NikaDeg <garelskaiavv1@gmail.com>
2025-11-18 09:28:08 +0200

    new

---
+++ Module3/module3-starters-main/t1/1.js
@@ -0,0 +1,11 @@
+'use strict';
+const div = document.querySelector('#target');
+const html =
+        `<p>
+            <li>First item</li>
+            <li>Second item</li>
+            <li>Third item</li>
+         </p>`;
+div.innerHTML = html;
+div.classList.add("my-list");
+
