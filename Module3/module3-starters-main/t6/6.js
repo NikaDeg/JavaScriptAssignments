fdd6a78c9b23de1ca41031bc0a18d41c2b70a918
NikaDeg <garelskaiavv1@gmail.com>
2025-11-19 20:50:01 +0200

    new

---
+++ Module3/module3-starters-main/t6/6.js
@@ -0,0 +1,7 @@
+'use strict';
+const button = document.querySelector('button');
+
+function popup(evt){
+  alert('Element ' + evt.currentTarget.tagName + ' was clicked');
+}
+button.addEventListener('click', popup);
